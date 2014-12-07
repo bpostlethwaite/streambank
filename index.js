138fee0dce98288b03437073c8734b1cdeaf37fe
bpostlethwaite <post.ben.here@gmail.com>
2014-12-07 17:38:57 -0500

    wat

--- index.js
+++ index.js
@@ -6,76 +6,74 @@
 module.exports = function () {
 
 
-    var self = {}
-      , StreamBank = {}
+    var self = {},
+        StreamBank = {};
 
 
     function addStream (stream, ID) {
 
         if (!(ID in StreamBank)) {
-            StreamBank[ID] = {}
-            StreamBank[ID].streams = []
+            StreamBank[ID] = {};
+            StreamBank[ID].streams = [];
         }
 
-        StreamBank[ID].streams.push(stream)
+        StreamBank[ID].streams.push(stream);
 
-        return StreamBank[ID].streams.length
+        return StreamBank[ID].streams.length;
 
     }
 
 
     function removeStream (stream, ID) {
 
-        if (!(ID in StreamBank)) return -1
-        var ix  = StreamBank[ID].streams.indexOf(stream)
-        if (ix < 0) return -1
-        StreamBank[ID].streams.splice(ix, 1)
+        if (!(ID in StreamBank)) return -1;
+        var ix  = StreamBank[ID].streams.indexOf(stream);
+        if (ix < 0) return -1;
+        StreamBank[ID].streams.splice(ix, 1);
 
         if (StreamBank[ID].streams.length === 0) {
-            delete StreamBank[ID]
-            return 0
+            delete StreamBank[ID];
+            return 0;
         }
 
-        return StreamBank[ID].streams.length
+        return StreamBank[ID].streams.length;
     }
 
 
 
     function forEachStream (ID, cb) {
 
-        if (!(ID in StreamBank)) return false
+        if (!(ID in StreamBank)) return false;
 
-        StreamBank[ID].streams.forEach(cb)
+        StreamBank[ID].streams.forEach(cb);
 
-        return true
+        return true;
     }
 
     function countStreamsByID (ID) {
-        var ConnCount = {}
+        var ConnCount = {};
         if (!ID) {
             for (ID in StreamBank) {
-                ConnCount[ID] = StreamBank[ID].streams.length
+                ConnCount[ID] = StreamBank[ID].streams.length;
             }
             return ConnCount;
         }
         else {
             if (!(ID in StreamBank)) {
-                return 0
+                return 0;
             }
             else {
-                return StreamBank[ID].streams.length
+                return StreamBank[ID].streams.length;
             }
         }
     }
 
-    function countStreamsForID (ID) {
 
-    }
+    self.addStream = addStream;
+    self.removeStream = removeStream;
+    self.forEachStream = forEachStream;
+    self.countStreamsByID = countStreamsByID;
+    self.bank = StreamBank;
 
-    self.addStream = addStream
-    self.removeStream = removeStream
-    self.forEachStream = forEachStream
-    self.countStreamsByID = countStreamsByID
-
-    return self
-}
+    return self;
+};
